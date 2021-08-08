c90ccff90daf58bf8568868eea329fefede69099
Jaime Alexis Garcia Hinojosa <alekzgh@gmail.com>
2021-08-07 22:09:43 -0500

    Primitive data samples

--- hola.ts
+++ hola.ts
@@ -6,3 +6,20 @@ function greeting( name: string ): string {
 }
 
 console.log( greeting("Alexis") );
+
+/* PRIMITIVE DATA */
+
+/* boolean */
+let logical: boolean = false;
+let isTrue: boolean = true;
+
+/* number */
+let anInteger: number = 13;
+let aFloating: number = 13.13;
+let anHexadecimal: number = 0xcbcbaa;
+let aBinary: number = 1101;
+let anExtremelyLongNumber: number = 13_130_130_131;
+// we can separate long numbers with an underscore to get a better understanding while we read 'em.
+
+/* strings */
+let aString: string = 'jasdanj2394das';
