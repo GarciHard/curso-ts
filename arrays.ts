7c4e63e180f426c357c9509a21720755daac0f03
Jaime Alexis Garcia Hinojosa <alekzgh@gmail.com>
2021-08-09 18:01:32 -0500

    Arrays and tumples review

---
+++ arrays.ts
@@ -0,0 +1,55 @@
+/* string array */
+let daysOfWeek: string[] = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+
+for (const element of daysOfWeek) {
+    console.log(element);
+}
+
+/* number array */
+let numbersArray: number[] = [1, 2, 3, 4, 5]
+
+for (const element of numbersArray) {
+    console.log(element);
+}
+
+/* any array */
+let anyArray: any = ["monday", "tuesday", "wednesday", "thursday", "friday", null, 1, undefined]
+
+for (const element of anyArray) {
+    console.log(element);
+}
+
+/* tuples */
+// they can be value typed and we can use i.e. as 'tmp' db.
+
+let db: [ name: string, 
+          age: number,
+          country: string,
+          activeUser: boolean ][] = [
+            [
+                "Alexis",
+                13,
+                "México",
+                true
+            ],
+            [
+                "Jaime",
+                14,
+                "México",
+                false
+            ],
+          ];
+
+// we can push elements.
+db.push( ["push", 1, "sample", false] );
+
+for (let elements of db) {
+    console.log(elements);
+}
+
+// removing elements from the array.
+db.pop();
+
+for (let elements of db) {
+    console.log(elements);
+}
