416540a996bb8498c8bc106a2c9879f608ea92ef
Jaime Alexis Garcia Hinojosa <alekzgh@gmail.com>
2021-08-15 17:30:41 -0500

    Ending of the classes on TS

--- classes.ts
+++ classes.ts
@@ -6,6 +6,12 @@ class Figure {
     private height: number;
     private width: number;
 
+    /* When we use the 'readonly' is like the 'final' word in java, we create an inmmutable attribute */
+
+    readonly anotherAttribute: number = 13;
+
+    private _figureName: string;
+
     constructor( height: number, width: number ) {
         this.height = height;
         this.width = width;
@@ -18,12 +24,23 @@ class Figure {
     perimeter() {
         return (this.height * 2) + (this.width * 2);
     }
+
+    getAnotherAttribute() {
+        return this.anotherAttribute;
+    }
+
+    get figureName() {
+        return this._figureName;
+    }
+
+    set figureName( value: string ) {
+        this._figureName = value;
+    }
 }
 
 let f1 = new Figure(5, 5);
 console.log(`f1 area -> ${f1.area()}`);
 console.log(`f1 perimeter -> ${f1.perimeter()}`);
-
-let f2 = new Figure(5, 10);
-console.log(`f2 area -> ${f2.area()}`);
-console.log(`f2 perimeter -> ${f2.perimeter()}`);
+console.log(`f1 anotherAttribute -> ${f1.getAnotherAttribute()}`);
+f1.figureName = "Rectangle";
+console.log(`f1 name -> ${f1.figureName}`);
